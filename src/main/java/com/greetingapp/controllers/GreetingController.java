d119e1abcba983abc7a400a5a61db4b66c260bae
kushagrashri7 <kushagrashrisharma@gmail.com>
2025-03-03 13:27:40 +0530

    [Kushagra Sharma] add . extend Greeting Class

---
+++ src/main/java/com/greetingapp/controllers/GreetingController.java
@@ -0,0 +1,43 @@
+package com.greetingapp.controllers;
+
+
+import com.greetingapp.services.GreetingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+
+    @Autowired
+    private GreetingService greetingService;
+
+    public static final Logger logger = LoggerFactory.getLogger(HTTPSControllers.class);
+
+    @GetMapping
+    public String getMethod(){
+        logger.info("Calling get Method");
+        return "{\"message\":\"" + greetingService.getGreetingMessage() + "\"}";
+    }
+
+    @PostMapping
+    public String postMethod(){
+        logger.info("Calling post Method");
+        return "{\"message\" : \""+greetingService.getGreetingMessage()+"\"}";
+    }
+
+    @PutMapping
+    public String putMethod(){
+        logger.info("Calling put Method");
+        return "{\"message\" : \""+greetingService.getGreetingMessage()+"\"}";
+    }
+
+    @DeleteMapping
+    public String deleteMethod(){
+        logger.info("Calling delete Method");
+        return "{\"message\" : \""+greetingService.getGreetingMessage()+"\"}";
+    }
+}
